3787d76d69f214003367e0d6d34616031c8f9135
José Guilherme <105402150+guilhermeverissimo14@users.noreply.github.com>
2023-03-30 16:56:58 -0300

    Finalizando quiz

--- src/pages/game/index.jsx
+++ src/pages/game/index.jsx
@@ -1,64 +1,65 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {questions, shuffle} from '../../database/questions'
+import { questions, shuffle } from '../../database/questions'
 
 import LogoQuiz from '../../assets/images/quiz.png'
 
 import './styles.css';
 import { useNavigate } from 'react-router-dom';
 
-function Game () {
+function Game() {
 
     const navigate = useNavigate();
-    const[currentQuestion, setCurrentQuestion] = useState(0);
-    const[questionSelected, setQuestionSelected] = useState([]);
-    const[userAnsweres, setUserAnsweres] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [questionSelected, setQuestionSelected] = useState([]);
+    const [userAnsweres, setUserAnsweres] = useState([]);
+    const [finish, setFinish] = useState(false);
 
-    useEffect(()=>{
-       if(questionSelected.length > 0)
-       return;
+    useEffect(() => {
+        if (questionSelected.length > 0)
+            return;
 
-       const embaralhadas = shuffle(questions)
-       const selectedQuestions = embaralhadas.slice(0,5);
+        const embaralhadas = shuffle(questions)
+        const selectedQuestions = embaralhadas.slice(0, 5);
+        setQuestionSelected(selectedQuestions)
+        console.log(selectedQuestions)
 
-       setQuestionSelected(selectedQuestions)
-       console.log(selectedQuestions)
+    }, [questionSelected])
 
-    },[questionSelected])
+    // função chamada quando o usuario responde uma questão
 
+    function handleAnsewer(selectedOptionIndex) {
+        const currentQuestionDAta = questionSelected[currentQuestion];
+        const userAnswer = currentQuestionDAta.answers[Number(selectedOptionIndex)];
+        setUserAnsweres([...userAnsweres, userAnswer])
+
+        if (currentQuestion < 4) {
+            setCurrentQuestion(currentQuestion + 1);
+        } else {
+            setFinish(true);
+        }
+    }
+
+    const currentQuestionDAta = questionSelected[currentQuestion];
     return (
         <div className="container">
-          <img src={LogoQuiz} alt="logo quiz"  className="logo"/>
+            <img src={LogoQuiz} alt="logo quiz" className="logo" />
 
-          <div className="card">
-             <div className="card-question">
-                 <h2 className="card-title">Pergunta 1 de 5</h2>
-                 <p className="question"> Qual a melhor linguagem de programação?</p>
-             </div>
-
-             <div className="card-answer">
-                <div className="card-options">
-                    <button className="card-option"> C++</button>
+            <div className="card">
+                <div className="card-question">
+                    <h2 className="card-title">Pergunta {currentQuestion +1} de 5</h2>
+                    <p className="question"> {currentQuestionDAta?.question}</p>
                 </div>
 
-                <div className="card-options">
-                    <button className="card-option"> Java</button>
+                <div className="card-answer">
+                    <div className="card-options">
+                        {currentQuestionDAta?.answers.map((option, index) => (
+                             <button onClick={()=>handleAnsewer(index)} key={index} className="card-option"> {option.text}</button>
+                        ))}
+                       
+                    </div>
                 </div>
-
-                <div className="card-options">
-                    <button className="card-option"> javascript</button>
-                </div>
-
-                <div className="card-options">
-                    <button className="card-option"> Python</button>
-                </div>
-
-                <div className="card-options">
-                    <button className="card-option"> C#</button>
-                </div>
-
-             </div>
-          </div>
+            </div>
         </div>
     )
 }
